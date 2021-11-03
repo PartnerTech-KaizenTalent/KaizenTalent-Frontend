0e98054463a3f801c8c93589bdd4876e301828a6
diegohcs96 <diego.hcs.96@gmail.com>
2021-11-03 12:46:13 -0500

    Update global-url.ts

--- src/app/util/global-url.ts
+++ src/app/util/global-url.ts
@@ -7,9 +7,9 @@ export const GlobalUrl = Object.freeze({
     //BASE_URL: 'http://192.168.5.144:2004/'
 
     //DEV Heroku URL
-    BASE_URL: 'https://kaizentalent-backend-dev.herokuapp.com/'
+    //BASE_URL: 'https://kaizentalent-backend-dev.herokuapp.com/'
     
     //QA Heroku URL
-    //BASE_URL: 'https://kaizentalent-backend.herokuapp.com/'
+    BASE_URL: 'https://kaizentalent-backend.herokuapp.com/'
 
 })
